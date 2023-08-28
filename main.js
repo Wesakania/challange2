ffd3cce5429168ccf51eb0add9db1a8ee5699575
Wesakania <wesakaniadavis@gmail.com>
2023-08-28 22:40:34 +0300

    my first challenge

---
+++ main.js
@@ -0,0 +1,21 @@
+//let MarksMass = 78;
+//let MarksHeight = 1.69;
+//let JohnsMass = 92;
+//let Johnheight = 1.95;
+
+//let bmiMarks = MarksMass / MarksHeight ** 2;
+
+//let bmiJohns = JohnsMass / Johnheight ** 2;
+
+//console.log(bmiMarks, bmiJohns);
+
+//console.log(bmiMarks > bmiJohns);
+let age = 31;
+if (age >= 35) {
+  console.log("Davis can acquire a driving license");
+} else {
+  let yearsLeft = 18 - age;
+  console.log(
+    `Davis is still too young to go for a driving. he should wait until ${yearsLeft} years before trying.`
+  );
+}
